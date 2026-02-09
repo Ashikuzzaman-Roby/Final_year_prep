02a8dfca2125a040f4fccc6d15411cddcc27a531
Ashikuzzaman Roby <mdroby3357@gmail.com>
2026-02-09 21:41:09 +0600

    prep has been started

--- Recursion/basiscs_recursion.py
+++ Recursion/basiscs_recursion.py
@@ -1,1 +1,9 @@
 print("Hello world")
+
+
+def number_conversion(n,b):
+    if n<b:
+        return n 
+    return str(number_conversion(n//b,b))+str(number_conversion(n%b,b))
+x=number_conversion(18,2)
+print(x)
