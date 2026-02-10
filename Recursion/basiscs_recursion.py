dea2e1085daceccf4075b0d320df9c6a14b75440
Ashikuzzaman Roby <mdroby3357@gmail.com>
2026-02-10 11:31:19 +0600

    w3 resources problem solving

--- Recursion/basiscs_recursion.py
+++ Recursion/basiscs_recursion.py
@@ -1,9 +1,32 @@
-print("Hello world")
+# solving 11 problem from w3 resources  
+
+# problem 01 : 
+# Sum of List Using Recursion
+
+# Write a Python program to calculate the sum of a list of numbers using recursion.
 
 
+# problem 01 : 
+lst = [7,3,8,4,2]
+def list_add(lst):
+    if len(lst)==1 : 
+        return lst[0]
+    return lst[0]+list_add(lst[1::])
+print(list_add(lst))
+
+
+
+
+
+
+
+
+# problem statement 02 : 
 def number_conversion(n,b):
+    string = "0123456789ABCDEF"
+
     if n<b:
-        return n 
-    return str(number_conversion(n//b,b))+str(number_conversion(n%b,b))
+        return string[n]
+    return number_conversion(n//b,b)+string[n%b]
 x=number_conversion(18,2)
 print(x)
